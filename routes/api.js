90b9302b0ba51bb8e4e05c8661f96ade22a35d9a
Antonio Marsari <antonio.marsari@gmail.com>
2025-03-29 22:40:49 -0300

    Complete api, started working on unit tests

--- routes/api.js
+++ routes/api.js
@@ -8,11 +8,65 @@ module.exports = function (app) {
 
   app.route('/api/check')
     .post((req, res) => {
+      const { puzzle, coordinate, value } = req.body;
+      if (!puzzle || !coordinate || !value) {
+        return res.json({ error: 'Required field(s) missing' });
+      }
 
+      if(solver.validate(puzzle) !== "Valid" ){
+        res.json({ error: solver.validate(puzzle) });
+        return;
+      }
+
+      const row = coordinate.split('')[0];
+      const column = coordinate.split('')[1];
+      if (coordinate.length !== 2 || !/[a-i]/i.test(row) || !/[1-9]/i.test(column)) {
+        res.json({ error: 'Invalid coordinate' });
+        return;
+      }
+      if(!/^[1-9]$/.test(value)){
+        res.json({ error: 'Invalid value' });
+        return;
+      }
+      let index = (solver.letterToNumber(row) - 1) * 9 + (parseInt(+column) - 1);
+      if (puzzle[index] == value) {
+        res.json({ valid: true });
+        return;
+      }
+
+      let validCol = solver.checkColPlacement(puzzle, row, column, value);
+      let validRegion = solver.checkRegionPlacement(puzzle, row, column, value);
+      let validRow = solver.checkRowPlacement(puzzle, row, column, value);
+      let conflicts = [];
+      if (validCol && validRow && validRegion) {
+        res.json({ valid: true });
+      } else {
+        if (!validRow) {
+          conflicts.push('row');
+        }
+        if (!validCol) {
+          conflicts.push('column');
+        }
+        if (!validRegion) {
+          conflicts.push('region');
+        }
+        res.json({ valid: false, conflict: conflicts });
+      }
     });
     
   app.route('/api/solve')
     .post((req, res) => {
-
+       const puzzle = req.body.puzzle;
+       if(solver.validate(puzzle) !== "Valid" ){
+        res.json({ error: solver.validate(puzzle) });
+        return;
+      }
+       const solution = solver.solve(puzzle);
+       if(!solution) {
+         res.json({ error: 'Puzzle cannot be solved' });
+         return;
+       }
+       res.json({ solution: solution });
+       return;
     });
 };
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -2,8 +2,24 @@ const chai = require('chai');
 const assert = chai.assert;
 
 const Solver = require('../controllers/sudoku-solver.js');
-let solver;
+let solver = new Solver();
 
+let validPuzzle = '82..4..6...16..89...98315.749.157.............53..4...96.415..81..7632..3...28.51';
 suite('Unit Tests', () => {
-
+    suite('Solver Tests', () => {
+        test('Valid puzzle string of 81 characters', () => {
+            let completePuzzle = '827549163531672894649831527496157382218396475753284916962415738185763249374928651';
+            assert.equal(solver.solve(validPuzzle), completePuzzle);
+        });
+        //continue from here
+        test('Invalid puzzle string length', () => {
+            assert.deepEqual(solver.validate('123456789'), { error: 'Expected puzzle to be 81 characters long' });
+        });
+        test('Invalid characters in puzzle string', () => {
+            assert.deepEqual(solver.validate('1234567890'), { error: 'Invalid characters in puzzle' });
+        });
+        test('Valid characters in puzzle string', () => {
+            assert.equal(solver.validate(validPuzzle), "Valid");
+        });
+    });
 });
